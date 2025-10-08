655fab7b434666c34b38e4dddbe3be05dc34ee47
unknown <atichol.rue@dome.tu.ac.th>
2025-10-08 23:25:32 +0700

    import libary for RoomRepository and add RoomRepositoryCustom.java for collect query

--- src/main/java/com/example/lc2_booking_room/repository/RoomRepository.java
+++ src/main/java/com/example/lc2_booking_room/repository/RoomRepository.java
@@ -1,5 +1,14 @@
 package com.example.lc2_booking_room.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Map;
+
+import com.example.lc2_booking_room.model.Room;
 @Repository
 public interface RoomRepository extends JpaRepository<Room, Long> {
 
