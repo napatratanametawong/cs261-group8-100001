2b0449b9b409bd51e3f2d925726a5bba54b32a7a
unknown <atichol.rue@dome.tu.ac.th>
2025-10-08 23:27:08 +0700

    import libary for RoomService.java

--- src/main/java/com/example/lc2_booking_room/service/RoomService.java
+++ src/main/java/com/example/lc2_booking_room/service/RoomService.java
@@ -1,12 +1,18 @@
 package com.example.lc2_booking_room.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Map;
+import com.example.lc2_booking_room.repository.RoomRepositoryCustom;
+
 @Service
 public class RoomService {
 
     @Autowired
-    private RoomRepository roomRepository;
+    private RoomRepositoryCustom roomRepositoryCustom;
 
     public List<Map<String, Object>> getRoomStatus(String date) {
-        return roomRepository.getRoomStatuses(date);
+        return roomRepositoryCustom.getRoomStatuses(date);
     }
 }
