8cfe7824690c845c813f4e71bf3473cf24785684
unknown <atichol.rue@dome.tu.ac.th>
2025-10-08 23:19:25 +0700

    import libary for RoomController.java

--- src/main/java/com/example/lc2_booking_room/controller/RoomController.java
+++ src/main/java/com/example/lc2_booking_room/controller/RoomController.java
@@ -1,5 +1,12 @@
 package com.example.lc2_booking_room.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+import com.example.lc2_booking_room.service.RoomService;
 @RestController
 @RequestMapping("/api/rooms")
 public class RoomController {
