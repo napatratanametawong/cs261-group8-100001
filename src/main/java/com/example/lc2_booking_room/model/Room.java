5fd9c296dca2bd647b3b57e4d627bf18573acfb6
unknown <atichol.rue@dome.tu.ac.th>
2025-10-17 20:54:58 +0700

    Change Variable to match with room.sql

--- src/main/java/com/example/lc2_booking_room/model/Room.java
+++ src/main/java/com/example/lc2_booking_room/model/Room.java
@@ -1,5 +1,6 @@
 package com.example.lc2_booking_room.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -7,15 +8,34 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
 
-@Data
 @Entity
 @Table(name = "rooms")
+@Data
 public class Room {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long roomId;
 
+    @Column(name = "code")
+    private String code;
+
+    @Column(name = "room_name")
     private String roomName;
+
+    @Column(name = "room_type")
     private String roomType;
-    private int capacity;
+
+    @Column(name = "min_capacity")
+    private Integer minCapacity;
+
+    @Column(name = "max_capacity")
+    private Integer maxCapacity;
+
+    @Column(name = "features_json")
+    private String featuresJson;
+
+    @Column(name = "active")
+    private Boolean active;
 }
+
