0b88639d714c95b03478523cd267a5ae1a3f2f71
unknown <atichol.rue@dome.tu.ac.th>
2025-10-17 20:06:42 +0700

    Change Variable type to match with file room.sql

--- src/main/java/com/example/lc2_booking_room/dto/room/CreateRoomRequest.java
+++ src/main/java/com/example/lc2_booking_room/dto/room/CreateRoomRequest.java
@@ -1,6 +1,9 @@
 package com.example.lc2_booking_room.dto.room;
 
 import lombok.Data;
+
+import java.util.List;
+
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -8,13 +11,35 @@ import jakarta.validation.constraints.NotNull;
 @Data
 public class CreateRoomRequest {
 
-    @NotBlank(message = "Room name is required")
+    // @NotBlank(message = "Room name is required")
+    // private String roomName;
+
+    // @NotBlank(message = "Room type is required")
+    // private String roomType;
+
+    // @NotNull(message = "Capacity is required")
+    // @Min(value = 1, message = "Capacity must be at least 1")
+    // private Integer capacity;
+
+    // public String getFeaturesJson() {
+    //     // TODO Auto-generated method stub
+    //     throw new UnsupportedOperationException("Unimplemented method 'getFeaturesJson'");
+    // }
+
+    // public Integer getMinCapacity() {
+    //     // TODO Auto-generated method stub
+    //     throw new UnsupportedOperationException("Unimplemented method 'getMinCapacity'");
+    // }
+
+    // public Integer getMaxCapacity() {
+    //     // TODO Auto-generated method stub
+    //     throw new UnsupportedOperationException("Unimplemented method 'getMaxCapacity'");
+    // }
+
     private String roomName;
-
-    @NotBlank(message = "Room type is required")
     private String roomType;
-
-    @NotNull(message = "Capacity is required")
-    @Min(value = 1, message = "Capacity must be at least 1")
-    private Integer capacity;
+    private int minCapacity;
+    private int maxCapacity;
+    private List<String> featuresJson;
 }
+
