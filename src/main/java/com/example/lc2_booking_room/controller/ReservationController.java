bcadcb7dd713e9f3dc62736380bf35ad357b1df8
unknown <atichol.rue@dome.tu.ac.th>
2025-11-09 01:56:23 +0700

    Add PutMapping to cancel reservation by reserevationID

--- src/main/java/com/example/lc2_booking_room/controller/ReservationController.java
+++ src/main/java/com/example/lc2_booking_room/controller/ReservationController.java
@@ -3,13 +3,8 @@ package com.example.lc2_booking_room.controller;
 import com.example.lc2_booking_room.dto.reservation.CreateReservationBySlotsRequest;
 import com.example.lc2_booking_room.dto.reservation.ReservationResponse;
 import com.example.lc2_booking_room.service.reservation.ReservationServiceBySlots;
-
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-
-import java.util.LinkedHashMap;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,17 +15,21 @@ public class ReservationController {
 
     private final ReservationServiceBySlots reservationService;
 
-    /** Create a reservation (room + date + multiple slotCodes) */
+    /** Create a reservation */
     @PostMapping
     public ResponseEntity<ReservationResponse> create(@Valid @RequestBody CreateReservationBySlotsRequest req) {
-        ReservationResponse res = reservationService.createReservationBySlots(req);
-        return ResponseEntity.ok(res);
+        return ResponseEntity.ok(reservationService.createReservationBySlots(req));
     }
 
-    /** Get reservation by ID (with slot details) */
+    /** Get reservation by ID */
     @GetMapping("/{id}")
     public ResponseEntity<ReservationResponse> getById(@PathVariable Long id) {
-        ReservationResponse res = reservationService.getById(id);
-        return ResponseEntity.ok(res);
+        return ResponseEntity.ok(reservationService.getById(id));
+    }
+
+    /** Cancel reservation (Task01-US07) */
+    @PutMapping("/{id}/cancel")
+    public ResponseEntity<ReservationResponse> cancelReservation(@PathVariable Long id) {
+        return ResponseEntity.ok(reservationService.cancelReservation(id));
     }
 }
