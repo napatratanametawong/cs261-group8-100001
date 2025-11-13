d06672a09fb276a9dccbfa41413bdfa390f419e1
Napat <napat.rata@dome.tu.ac.th>
2025-11-13 23:37:14 +0700

    integration header bar into resrvation.html

--- src/main/resources/static/header/scripts/header.js
+++ src/main/resources/static/header/scripts/header.js
@@ -1,27 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const headerHTML = `
-        <div class="top-bar">
-            <div class="brand-logo">
-                <img src="../resource/Logo_header.png" alt="Logo" height="56" />
-            </div>
+  const headerHTML = `
+   <div class="top-bar">
+    <div class="brand-logo">
+      <img src="../../../resource/Logo_header.png" alt="Logo" height="56" />
+    </div>
+    <div class="toolbar">
+      <button class="icon-btn" title="การแจ้งเตือน">
+        <img src="../../../resource/bell.svg" alt="การแจ้งเตือน" />
+      </button>
+      <div class="chip">
+        <span id="displayName" class="displayName">ณภัทร รัตนเมธาวงศ์</span>
+      </div>
+      <button class="icon-btn" id="logout_btn" title="ออกจากระบบ">
+        <img src="../../../resource/logout.svg" alt="ออกจากระบบ" />
+      </button>
+    </div>
+  </div>
 
-            <div class="toolbar">
-                <button class="icon-btn" title="การแจ้งเตือน">
-                    <img src="../resource/bell.svg" alt="การแจ้งเตือน" />
-                </button>
+  <header class="app"></header>
+  `;
 
-                <div class="chip">
-                    <span id="displayName" class="displayName"></span>
-                </div>
-
-                <button class="icon-btn" id="logout_btn" title="ออกจากระบบ">
-                    <img src="../resource/logout.svg" alt="ออกจากระบบ" />
-                </button>
-        
-            </div>
-        </div>
-        
-    `;
-
-    document.body.insertAdjacentHTML("afterbegin", headerHTML);
+  // แทรก header เข้าไปที่ด้านบนสุดของ <body>
+  document.body.insertAdjacentHTML("afterbegin", headerHTML);
 });
