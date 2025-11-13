34fbd96ee06c0d6546bd7d8c46f20071a9df8f59
Napat <napat.rata@dome.tu.ac.th>
2025-11-13 17:33:19 +0700

    insert header on top of page

--- src/main/resources/static/header/scripts/header.js
+++ src/main/resources/static/header/scripts/header.js
@@ -1,20 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const headerHTML = `
-    <div class="top-bar">
-      <div class="brand-logo">
-        <img src="../resource/Logo_header.png" alt="Logo" />
-      </div>
-      <div class="toolbar">
-        <button class="icon-btn" title="แจ้งเตือน">
-          <img src="../resource/bell.svg" alt="แจ้งเตือน" />
-        </button>
-        <div class="chip">
-          <span id="displayName" class="displayName">Guest</span>
-        </div>
-        <button class="icon-btn" id="logout_btn" title="ออก">
-          <img src="../resource/logout.svg" alt="ออก" />
-        </button>
-      </div>
-    </div>
+    const headerHTML = `
+        <div class="top-bar">
+            <div class="brand-logo">
+                <img src="../resource/Logo_header.png" alt="Logo" height="56" />
+            </div>
 
+            <div class="toolbar">
+                <button class="icon-btn" title="การแจ้งเตือน">
+                    <img src="../resource/bell.svg" alt="การแจ้งเตือน" />
+                </button>
+
+                <div class="chip">
+                    <span id="displayName" class="displayName"></span>
+                </div>
+
+                <button class="icon-btn" id="logout_btn" title="ออกจากระบบ">
+                    <img src="../resource/logout.svg" alt="ออกจากระบบ" />
+                </button>
+                
+            </div>
+        </div>
+`;
+
+    document.body.insertAdjacentHTML("afterbegin", headerHTML);
 });
